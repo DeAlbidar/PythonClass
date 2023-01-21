f002db543691720d901468d3154f520c797ecfc6
DeAlbidar <albidarebenezernarh@gmail.com>
2023-01-21 08:55:27 +0000

    Loops

--- loops.py
+++ loops.py
@@ -1,7 +1,6 @@
 # While loop
 # Assignment 1
 # Guessing Game
-#
 
 def guess():
     guess_limit = 3
@@ -17,4 +16,50 @@ def guess():
             print("Wrong Guess, try again...")
     else:
         print("Sorry you failed!")
+
+
+# While loop
+# Assignment 2
+# Car Game
+#
+# help
+#   start - to start the car
+#   stop - to stop the car
+#   quit - to exit
+
+def car():
+    car_status = False
+    car_stop = False
+    while True:
+        command = input("> ").lower()
+        if command == "start":
+            if not car_status:
+                car_status = True
+                car_stop = False
+                print("Car started... Ready to go!")
+            else:
+                print("Car already started...")
+        elif command == "stop":
+            if not car_status:
+                print("Car not started...")
+            else:
+                if not car_stop:
+                    car_stop = True
+                    car_status = False
+                    print("Car stopped.")
+                else:
+                    print("Car already stopped.")
+        elif command == "help":
+            print("start - to start the car")
+            print("stop - to stop the car")
+            print("quit - to exit")
+        elif command == "quit":
+            break
+        else:
+            print("Sorry, I don't understand that...")
+
+
+# For Loops
+# 
+
 
