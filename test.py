b13fcc09a10596eb820864a6049054ef5d0015e1
DeAlbidar <albidarebenezernarh@gmail.com>
2023-02-04 12:54:06 +0000

    Update

---
+++ test.py
@@ -0,0 +1,32 @@
+# Given a string S, return the "reversed" string where all characters that are not a letter stay
+# in the same place, and all letters reverse their positions.
+#
+#
+# Example 1:
+#
+# Input: "ab-cd"
+#
+# Output: "dc-ba"
+#
+#
+# Example 2:
+#
+# Input: "a-bC-dEf=ghIj!!"
+#
+# Output: "j-Ih-gfE=dCba!!"
+
+def reverseOnlyLetters(S: str) -> str:
+    result = ''
+
+    return result
+
+
+# R E A D M E
+# DO NOT CHANGE the code below, we use it to grade your submission.
+# If changed your submission will be failed automatically.
+
+if __name__ == "__main__":
+    S = input().strip()
+
+    print(reverseOnlyLetters(S))
+
